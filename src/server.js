17cd85e6ac8d283e8d22e47771bf6fd6a174c948
Davy Peter Braun <hello@davybraun.com>
2016-01-06 10:13:21 +0100

    Ensure that when a client connects, it receives the current state of the app.

--- src/server.js
+++ src/server.js
@@ -4,4 +4,6 @@ export default function startServer(store) {
   const io = new Server().attach(8090);
 
   store.subscribe(() => io.emit('state', store.getState().toJS()));
+
+  io.on('connection', socket => socket.emit('state', store.getState().toJS()));
 }
